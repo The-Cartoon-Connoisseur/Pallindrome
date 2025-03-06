371e73c28d30c7f813191e34fe4c370479e0a2f7
Lucas Hester <lucashester@icloud.com>
2025-03-06 13:23:17 -0500

    Test 1
    Raises a Value Error when not provided with a value that is an instance of str

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,22 @@
 """
 Validates strings as palindromes.
 """
+
+from collections import deque
+
+def is_palindrome(value: str) -> bool:
+    if not  isinstance(value, str):
+        raise ValueError("Input must be a string")
+
+    if value == "":
+        return False
+
+    normalized = "".join(c.lower() for c in value if c.isalnum())
+
+    char_deque = deque(normalized)
+
+    while len(char_deque) > 1:
+        if char_deque.popleft() != char_deque.pop():
+            return False
+
+        return True
--- test_palindrome.py
+++ test_palindrome.py
@@ -1,3 +1,11 @@
 """
 Tests the palindrome module
 """
+import unittest
+from palindrome import is_palindrome
+
+class TestPalindrome(unittest.TestCase):
+
+    def test_raises_value_error_for_non_string(self):
+        with self.assertRaises(ValueError):
+            is_palindrome(123)
