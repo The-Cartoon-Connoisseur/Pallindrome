7f650434ac6008b7eddb652ef5c13abf8182f9d4
Lucas Hester <lucashester@icloud.com>
2025-03-06 13:26:32 -0500

    Test 2
    Returns False when called with an empty string

--- test_palindrome.py
+++ test_palindrome.py
@@ -6,6 +6,11 @@ from palindrome import is_palindrome
 
 class TestPalindrome(unittest.TestCase):
 
+#Test 1
     def test_raises_value_error_for_non_string(self):
         with self.assertRaises(ValueError):
             is_palindrome(123)
+
+#Test 2
+    def test_empty_string_returns_false(self):
+        self.assertFalse(is_palindrome(""))
